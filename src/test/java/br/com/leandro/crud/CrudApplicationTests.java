0392da3fd423722b4d4616d45dbeb819e0cc8a81
leandro-jesus-eng <leandro.jesus.eng@gmail.com>
2021-05-06 15:13:36 -0400

    generate first version of JAR file

--- src/test/java/br/com/leandro/crud/CrudApplicationTests.java
+++ src/test/java/br/com/leandro/crud/CrudApplicationTests.java
@@ -3,7 +3,7 @@ package br.com.leandro.crud;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest (classes = CrudPersonApplication.class)
 class CrudApplicationTests {
 
 	@Test
